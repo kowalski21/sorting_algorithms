ec94e72e37a7d09b0613c11ba12d241ec7e31391
Douglas Amoo-Sargon <douglasbiomed3@gmail.com>
2023-09-25 21:45:59 -0700

    second change

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,4 +1,29 @@
+/*
+ * File: 1-insertion_sort_list.c
+ */
+
 #include "sort.h"
+
+/**
+ * swap_nodes - Swap two nodes in a listint_t doubly-linked list.
+ * @h: A pointer to the head of the doubly-linked list.
+ * @n1: A pointer to the first node to swap.
+ * @n2: The second node to swap.
+ */
+void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
+{
+	(*n1)->next = n2->next;
+	if (n2->next != NULL)
+		n2->next->prev = *n1;
+	n2->prev = (*n1)->prev;
+	n2->next = *n1;
+	if ((*n1)->prev != NULL)
+		(*n1)->prev->next = n2;
+	else
+		*h = n2;
+	(*n1)->prev = n2;
+	*n1 = n2->prev;
+}
 
 /**
  * insertion_sort_list - Sorts a doubly linked list of integers
@@ -9,18 +34,18 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *param_l, *tmp_l, *track_i;
+	listint_t *iter, *insert, *tmp;
 
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
-	for (track_i = (*list)->next; track_i != NULL; track_i = tmp_l)
+	for (iter = (*list)->next; iter != NULL; iter = tmp)
 	{
-		tmp_l = track_i->next;
-		param_l = track_i->prev;
-		while (param_l != NULL && track_i->n < param_l->n)
+		tmp = iter->next;
+		insert = iter->prev;
+		while (insert != NULL && iter->n < insert->n)
 		{
-			swap_nodes(list, &param_l, track_i);
+			swap_nodes(list, &insert, iter);
 			print_list((const listint_t *)*list);
 		}
 	}
--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,5 +1,22 @@
+/*
+ * File: 2-selection_sort.c
+ */
+
 #include "sort.h"
 
+/**
+ * swap_ints - Swap two integers in an array.
+ * @a: The first integer to swap.
+ * @b: The second integer to swap.
+ */
+void swap_ints(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
 
 /**
  * selection_sort - Sort an array of integers in ascending order
@@ -11,21 +28,21 @@
  */
 void selection_sort(int *array, size_t size)
 {
-	int *tracker;
-	size_t inner_count, outer_count;
+	int *min;
+	size_t i, j;
 
 	if (array == NULL || size < 2)
 		return;
 
-	for (inner_count = 0; inner_count < size - 1; inner_count++)
+	for (i = 0; i < size - 1; i++)
 	{
-		tracker = array + inner_count;
-		for (outer_count = inner_count + 1; outer_count < size; outer_count++)
-			tracker = (array[outer_count] < *tracker) ? (array + outer_count) : tracker;
+		min = array + i;
+		for (j = i + 1; j < size; j++)
+			min = (array[j] < *min) ? (array + j) : min;
 
-		if ((array + inner_count) != tracker)
+		if ((array + i) != min)
 		{
-			swap_array_ints(array + inner_count, tracker);
+			swap_ints(array + i, min);
 			print_array(array, size);
 		}
 	}
