1e5c26d97372d508e473a122f56959994eb72356
Douglas Amoo-Sargon <douglasbiomed3@gmail.com>
2023-09-26 00:58:24 -0700

    fifth fix

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,5 +1,9 @@
-#include "sort.h"
+/*
+ * File: 0-bubble_sort.c
+ * Auth: Brennan D Baraban
+ */
 
+#include "sort.h"
 /**
  * swap_arr_int - Swap two integers in an array.
  * @num_1: The first integer to swap.
@@ -23,22 +27,22 @@ void swap_arr_int(int *num_1, int *num_2)
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t counter, arr_len = size;
-	bool bubbly = false;
+	size_t i, arr_len = size;
+	bool checker = false;
 
 	if (array == NULL || size < 2)
 		return;
 
-	while (bubbly == false)
+	while (checker == false)
 	{
-		bubbly = true;
-		for (counter = 0; counter < arr_len - 1; counter++)
+		checker = true;
+		for (i = 0; i < arr_len - 1; i++)
 		{
-			if (array[counter] > array[counter + 1])
+			if (array[i] > array[i + 1])
 			{
-				swap_arr_int(array + counter, array + counter + 1);
+				swap_arr_int(array + i, array + i + 1);
 				print_array(array, size);
-				bubbly = false;
+				checker = false;
 			}
 		}
 		arr_len--;
